4905404b3d1a2f671d0ff60cb013a96c07feb05d
Nelson Santos <nbsantos@gmail.com>
2018-09-19 11:56:42 -0400

    PAPP-4949 Added filter to "list tickets" action and create "on poll" action.

--- servicenow_consts.py
+++ servicenow_consts.py
@@ -33,6 +33,8 @@ SERVICENOW_JSON_INGEST_MANNER = "ingest_manner"
 SERVICENOW_JSON_MAX_ITEMS = "max_items"
 SERVICENOW_JSON_VAULT_ID = "vault_id"
 SERVICENOW_JSON_FILTER = "filter"
+SERVICENOW_JSON_ON_POLL_FILTER = "on_poll_filter"
+SERVICENOW_JSON_ON_POLL_TABLE = "on_poll_table"
 
 SERVICENOW_ERR_API_INITIALIZATION = "API Initialization failed"
 SERVICENOW_ERR_CONNECTIVITY_TEST = "Connectivity test failed"
@@ -47,6 +49,7 @@ SERVICENOW_ERR_FIELDS_JSON_PARSE = "Unable to parse the fields parameter into a 
 SERVICENOW_ERR_API_UNSUPPORTED_METHOD = "Unsupported method"
 SERVICENOW_ERR_EMPTY_FIELDS = "The fields dictionary was detected to be empty"
 SERVICENOW_ERR_ONE_PARAM_REQ = "Please specify at least one of the parameters short_description, description, or fields to create the ticket with"
+SERVICENOW_ERR_FAILURES = "Some tickets had issues during ingestion, see logs for details."
 
 SERVICENOW_CREATED_TICKET = "Created ticket"
 SERVICENOW_USING_BASE_URL = "Using url: {base_url}"
@@ -55,7 +58,12 @@ SERVICENOW_ERR_JSON_PARSE = "Unable to parse reply as a Json, raw string reply: 
 DEFAULT_MAX_RESULTS = 100
 SERVICENOW_TICKET_FOOTNOTE = "Added by Phantom for container id: "
 SERVICENOW_DEFAULT_TABLE = "incident"
+ON_POLL_MAX_RESULTS = 10000
 
 SERVICENOW_MAX_COUNT_VALUE = 4294967295
 SERVICENOW_INGEST_LATEST_ITEMS = "latest first"
 SERVICENOW_INGEST_OLDEST_ITEMS = "oldest first"
+
+SERVICENOW_ITEM_OPT_MTOM_TABLE = "sc_item_option_mtom"
+SERVICENOW_ITEM_OPT_TABLE = "sc_item_option"
+SERVICENOW_ITEM_OPT_NEW_TABLE = "item_option_new"
