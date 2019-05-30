cc69ffb4feae21815d267ae22c210d8091650b78
Jay Patel <jay.patel@M06-JayP.local>
2019-05-30 16:49:01 +0530

    ServiceNow: Commit for the pipeline failures resolutions

--- servicenow_view.py
+++ servicenow_view.py
@@ -1,5 +1,5 @@
-# File: abuseipdb_view.py
-# Copyright (c) 2017-2019 Splunk Inc.
+# File: servicenow_view.py
+# Copyright (c) 2016-2019 Splunk Inc.
 #
 # SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
 # without a valid written license from Splunk Inc. is PROHIBITED.
