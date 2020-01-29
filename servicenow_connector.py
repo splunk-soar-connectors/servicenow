3f0048cb5489eb32d315163afab1b874f7dfd9d3
Ricardo Munoz <rmunoz@rmunoz-mbp-9d822.sv.splunk.com>
2020-01-29 14:59:48 -0800

    first try at py3

--- servicenow_connector.py
+++ servicenow_connector.py
@@ -1065,7 +1065,7 @@ class ServicenowConnector(BaseConnector):
             invalid_variables = mandatory_variables
         else:
             for var in mandatory_variables:
-                if var not in variables_param.keys():
+                if var not in list(variables_param.keys()):
                     invalid_variables = mandatory_variables
                     break
 
@@ -1527,20 +1527,65 @@ class ServicenowConnector(BaseConnector):
 
 if __name__ == '__main__':
 
-    import sys
-    # import simplejson as json
     import pudb
+    import argparse
 
     pudb.set_trace()
 
-    with open(sys.argv[1]) as f:
+    argparser = argparse.ArgumentParser()
+
+    argparser.add_argument('input_test_json', help='Input Test JSON file')
+    argparser.add_argument('-u', '--username', help='username', required=False)
+    argparser.add_argument('-p', '--password', help='password', required=False)
+
+    args = argparser.parse_args()
+    session_id = None
+
+    username = args.username
+    password = args.password
+
+    if (username is not None and password is None):
+
+        # User specified a username but not a password, so ask
+        import getpass
+        password = getpass.getpass("Password: ")
+
+    if (username and password):
+        try:
+            print ("Accessing the Login page")
+            login_url = BaseConnector._get_phantom_base_url() + "login"
+            r = requests.get(login_url, verify=False)
+            csrftoken = r.cookies['csrftoken']
+
+            data = dict()
+            data['username'] = username
+            data['password'] = password
+            data['csrfmiddlewaretoken'] = csrftoken
+
+            headers = dict()
+            headers['Cookie'] = 'csrftoken=' + csrftoken
+            headers['Referer'] = BaseConnector._get_phantom_base_url() + 'login'
+
+            print ("Logging into Platform to get the session id")
+            r2 = requests.post(login_url, verify=False, data=data, headers=headers)
+            session_id = r2.cookies['sessionid']
+        except Exception as e:
+            print ("Unable to get session id from the platfrom. Error: " + str(e))
+            exit(1)
+
+    with open(args.input_test_json) as f:
         in_json = f.read()
         in_json = json.loads(in_json)
         print(json.dumps(in_json, indent=4))
 
         connector = ServicenowConnector()
         connector.print_progress_message = True
+
+        if (session_id is not None):
+            in_json['user_session_token'] = session_id
+            connector._set_csrf_info(csrftoken, headers['Referer'])
+
         ret_val = connector._handle_action(json.dumps(in_json), None)
-        print ret_val
+        print (json.dumps(json.loads(ret_val), indent=4))
 
     exit(0)
