4c94be34b35d369234eb0630945c498c2739e36b
jpatel-crest <jpatel@splunk.com>
2020-05-03 13:10:28 +0530

    ServiceNow - Commit for the PAPP-9914

--- servicenow_consts.py
+++ servicenow_consts.py
@@ -17,15 +17,10 @@ SERVICENOW_JSON_SHORT_DESCRIPTION = "short_description"
 SERVICENOW_JSON_DESCRIPTION = "description"
 SERVICENOW_JSON_NEW_TICKET_ID = "created_ticket_id"
 SERVICENOW_JSON_GOT_TICKET_ID = "queried_ticket_id"
-SERVICENOW_JSON_UPDATED_TICKET_ID = "updated_ticket_id"
 SERVICENOW_JSON_SYS_ID = "sys_id"
 SERVICENOW_JSON_TICKET_ID = "id"
 SERVICENOW_JSON_FIELDS = "fields"
 SERVICENOW_JSON_TABLE = "table"
-SERVICENOW_JSON_FILTER_QUERY = "filter_query"
-SERVICENOW_JSON_FIRST_RUN_MAX_ITEMS = "first_run_max_items"
-SERVICENOW_JSON_INGEST_MANNER = "ingest_manner"
-SERVICENOW_JSON_MAX_ITEMS = "max_items"
 SERVICENOW_JSON_VAULT_ID = "vault_id"
 SERVICENOW_JSON_FILTER = "filter"
 SERVICENOW_JSON_ON_POLL_FILTER = "on_poll_filter"
@@ -36,13 +31,10 @@ SERVICENOW_JSON_EXTRACT_IPS = "extract_ips"
 SERVICENOW_JSON_EXTRACT_HASHES = "extract_hashes"
 SERVICENOW_JSON_EXTRACT_URLS = "extract_urls"
 
-SERVICENOW_ERR_API_INITIALIZATION = "API Initialization failed"
 SERVICENOW_ERR_CONNECTIVITY_TEST = "Test Connectivity Failed"
 SERVICENOW_SUCC_CONNECTIVITY_TEST = "Test Connectivity Passed"
-SERVICENOW_ERR_CREATE_TICKET_FAILED = "Ticket creation failed"
-SERVICENOW_SUCC_TICKET_CREATED = "Created ticket with key: {key}"
-SERVICENOW_ERR_LIST_TICKETS_FAILED = "Failed to get ticket listing"
 SERVICENOW_ERR_SERVER_CONNECTION = "Connection failed"
+SERVICENOW_LIMIT_VALIDATION_MSG = "Please provide a valid non-zero positive integer value in the '{parameter}' parameter"
 SERVICENOW_ERR_FETCH_VALUE = 'Error occurred while fetching variable value for the item_option_value: {item_opt_value} of the System ID: {sys_id}'
 SERVICENOW_ERR_FETCH_QUESTION_ID = 'Error occurred while fetching question ID for the item_option_value: {item_opt_value} of the System ID: {sys_id}'
 SERVICENOW_ERR_FETCH_QUESTION = 'Error occurred while fetching question for the question ID: {question_id} and the item_option_value: {item_opt_value} of the System ID: {sys_id}'
@@ -51,23 +43,15 @@ SERVICENOW_MSG_GET_INCIDENT_TEST = "Querying a single Incident to check credenti
 SERVICENOW_ERR_FIELDS_JSON_PARSE = "Unable to parse the fields parameter into a dictionary"
 SERVICENOW_ERR_API_UNSUPPORTED_METHOD = "Unsupported method"
 SERVICENOW_ERR_BASIC_AUTH_NOT_GIVEN_FIRST_TIME = 'Provide username and password to generate OAuth token for running Test Connectivity for the first time'
-SERVICENOW_ERR_EMPTY_FIELDS = "The fields dictionary was detected to be empty"
 SERVICENOW_ERR_ONE_PARAM_REQ = "Please specify at least one of the parameters short_description, description, or fields to create the ticket with"
 SERVICENOW_ERR_FAILURES = "Some tickets had issues during ingestion, see logs for details"
-SERVICENOW_ERR_INVALID_PARAM = "Please provide non-zero positive integer in '{param}'"
 
-SERVICENOW_CREATED_TICKET = "Created ticket"
 SERVICENOW_USING_BASE_URL = "Using url: {base_url}"
-SERVICENOW_ERR_JSON_PARSE = "Unable to parse reply as a Json, raw string reply: '{raw_text}'"
 SERVICENOW_BASE_QUERY_URI = "/table/"
 
 DEFAULT_MAX_RESULTS = 100
 SERVICENOW_TICKET_FOOTNOTE = "Added by Phantom for container id: "
 SERVICENOW_DEFAULT_TABLE = "incident"
-
-SERVICENOW_MAX_COUNT_VALUE = 4294967295
-SERVICENOW_INGEST_LATEST_ITEMS = "latest first"
-SERVICENOW_INGEST_OLDEST_ITEMS = "oldest first"
 
 SERVICENOW_ITEM_OPT_MTOM_TABLE = "sc_item_option_mtom"
 SERVICENOW_ITEM_OPT_TABLE = "sc_item_option"
