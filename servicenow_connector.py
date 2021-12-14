6127092b94c3035d8db4581c38563c3742babfea
Harsh Srivastava <hsrivastava@splunk.com>
2021-12-14 12:25:35 +0530

    Made changes for the semgrep handling

--- servicenow_connector.py
+++ servicenow_connector.py
@@ -1933,12 +1933,14 @@ if __name__ == '__main__':
     argparser.add_argument('input_test_json', help='Input Test JSON file')
     argparser.add_argument('-u', '--username', help='username', required=False)
     argparser.add_argument('-p', '--password', help='password', required=False)
+    argparser.add_argument('-v', '--verify', action='store_true', help='verify', required=False, default=False)
 
     args = argparser.parse_args()
     session_id = None
 
     username = args.username
     password = args.password
+    verify = args.verify
 
     if username is not None and password is None:
 
@@ -1950,7 +1952,7 @@ if __name__ == '__main__':
         try:
             print("Accessing the Login page")
             login_url = '{}{}'.format(BaseConnector._get_phantom_base_url(), "login")
-            r = requests.get(login_url, verify=False)
+            r = requests.get(login_url, verify=verify)
             csrftoken = r.cookies['csrftoken']
 
             data = dict()
@@ -1963,11 +1965,11 @@ if __name__ == '__main__':
             headers['Referer'] = '{}{}'.format(BaseConnector._get_phantom_base_url(), 'login')
 
             print("Logging into Platform to get the session id")
-            r2 = requests.post(login_url, verify=False, data=data, headers=headers)
+            r2 = requests.post(login_url, verify=verify, data=data, headers=headers)
             session_id = r2.cookies['sessionid']
         except Exception as e:
             print("Unable to get session id from the platfrom. Error: {}".format(str(e)))
-            exit(1)
+            sys.exit(1)
 
     with open(args.input_test_json) as f:
         in_json = f.read()
@@ -1984,4 +1986,4 @@ if __name__ == '__main__':
         ret_val = connector._handle_action(json.dumps(in_json), None)
         print(json.dumps(json.loads(ret_val), indent=4))
 
-    exit(0)
+    sys.exit(0)
